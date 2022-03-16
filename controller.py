fae4f57e403d9e7a30d088606a31ad2d0eca1db0
yanax <yanax@mail.ru>
2022-03-16 13:37:58 +0300

    add import_datcontroller.pya.py

--- controller.py
+++ controller.py
@@ -1,4 +1,10 @@
-import user_input, import_data
+import user_input, import_data, export
 
+def import_data_controller(data):
+    import_data.row_imp(data)
+    import_data.rows_imp(data)
 
-import_data.row_imp(user_input.input_data())
+import_data_controller(user_input.input_data())
+
+# def export_data_controller(data):
+#     if
